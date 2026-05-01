2bba6458a3ff671e4170cdd62881048c4440de90
ripureityu <t.yudai20081015@gmail.com>
2026-05-01 07:53:54 +0000

    jsファイルとvite

---
+++ main.js
@@ -0,0 +1,10 @@
+import { someFunc } from './module.js';
+someFunc();
+// Import Blockly core.
+import * as Blockly from 'blockly/core';
+// Import the default blocks.
+import * as libraryBlocks from 'blockly/blocks';
+// Import a generator.
+import {javascriptGenerator} from 'blockly/javascript';
+// Import a message file.
+import * as En from 'blockly/msg/en';
